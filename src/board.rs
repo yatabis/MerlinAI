07bd717ae9f37c932089b41106738538fbc1befb
yatabis <39yatabis.rim8820xxx@gmail.com>
2022-02-05 17:47:13 +0900

    カレントミノの出現

--- src/board.rs
+++ src/board.rs
@@ -14,6 +14,14 @@ pub enum Mino {
 
 pub const MINO_LIST: [Mino; 7] = [Mino::I, Mino::O, Mino::S, Mino::Z, Mino::J, Mino::L, Mino::T];
 
+const I_SPAWN: u64 = 0x0000000007800000;
+const O_SPAWN: u64 = 0x0000000C03000000;
+const S_SPAWN: u64 = 0x0000000C01800000;
+const Z_SPAWN: u64 = 0x0000000603000000;
+const J_SPAWN: u64 = 0x0000000203800000;
+const L_SPAWN: u64 = 0x0000000803800000;
+const T_SPAWN: u64 = 0x0000000403800000;
+
 enum Rotation {
     North,
     East,
@@ -43,4 +51,25 @@ impl Board {
             }
         }
     }
-}
+
+    pub fn spawn(&mut self, mino: Mino) {
+        self.current = MinoInfo {
+            mino,
+            rotation: Rotation::North,
+            board: [
+                0, 0, 0, match mino {
+                    Mino::I => I_SPAWN,
+                    Mino::O => O_SPAWN,
+                    Mino::S => S_SPAWN,
+                    Mino::Z => Z_SPAWN,
+                    Mino::J => J_SPAWN,
+                    Mino::L => L_SPAWN,
+                    Mino::T => T_SPAWN,
+                    Mino::None => 0,
+                }
+            ]
+        };
+        if self.current.board[3] >> 10 & self.field[3] == 0 {
+            self.current.board[3] >>= 10;
+        }
+    }
